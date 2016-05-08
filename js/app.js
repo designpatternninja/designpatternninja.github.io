d747ea4c38e22ce7bb3467977b52f2e4f91ddbc3
mfidemraizer <fidemraizer@hotmail.com>
2016-05-08 12:31:37 +0200

    UI improvements
    Now opened design pattern is marked as selected with green color in
    side-app menu. And now when an article is opened, either patterns or
    news, its revealed with some slide-in animation

--- js/app.js
+++ js/app.js
@@ -5,7 +5,18 @@
 
 	});
 
-	app.controller("MasterController", function() {
+	app.controller("MasterController", function($scope, $http) {
+		this.issues = [];
+		this.patterns = {};
+		var that = this;
 
+		this.currentPath = window.location.pathname;
+
+		this.sideAppMenuSelectedTabIndex = window.location.pathname.indexOf('/news/') > -1 ? 0 : 1;
+		/*if(window.location.pathname.indexOf("/news/") == -1)
+			$http.get("https://api.github.com/repos/designpatternninja/designpatternninja.github.io/issues?labels=singleton&filter=all")
+				.success(function(issues) {
+					that.issues = issues;
+				});*/
 	});
 })();
