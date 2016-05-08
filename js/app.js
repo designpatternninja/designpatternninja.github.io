c5067c21c92b914932bfa9c15309f30789b1501b
mfidemraizer <fidemraizer@hotmail.com>
2016-05-08 23:53:30 +0200

    More UI improvements
    Now both design pattern-related tabs have counters. In the other hand,
    I've added a new tab "Anti-patterns" to document bad solutions and wrong
    habits too!

--- js/app.js
+++ js/app.js
@@ -12,7 +12,13 @@
 
 		this.currentPath = window.location.pathname;
 
-		this.sideAppMenuSelectedTabIndex = window.location.pathname.indexOf('/news/') > -1 ? 0 : 1;
+		if(window.location.pathname.indexOf("/news/") > -1)
+			this.sideAppMenuSelectedTabIndex = 0;
+		else if(window.location.pathname.indexOf("/anti-pattern/") > -1)
+			this.sideAppMenuSelectedTabIndex = 2;
+		else
+			this.sideAppMenuSelectedTabIndex = 1;
+			
 		/*if(window.location.pathname.indexOf("/news/") == -1)
 			$http.get("https://api.github.com/repos/designpatternninja/designpatternninja.github.io/issues?labels=singleton&filter=all")
 				.success(function(issues) {
